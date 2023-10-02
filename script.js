8fc36e255bcb3eb322fe213ed67a2d4cfb115966
MikeHill89 <mvreugdenhil18@gmail.com>
2023-10-02 17:56:12 +0300

    Setup Book object constructor and prototype function. Added ability to add created instances of Book to myLibrary array

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("test");
+const myLibrary = [];
+
+//constructor of book objects, creates instances of Book
+function Book(title,author,pages,status){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+};
+
+Book.prototype.returnBookInfo = function () {
+    return `Title: ${this.title}, written by ${this.author}, Pages: ${this.pages}, Read: ${this.status}.`;
+};
+
+function addBookToLibrary(){
+    //do stuff here
+    let booktitle = prompt("What is the title of the book?:");
+    let bookauthor = prompt("Who is the author of the book?:")
+    let bookpages = prompt("How many pages is the book?:")
+    let bookstatus = prompt("Have you completed the book?:")
+
+    const createBook = new Book(booktitle,bookauthor,bookpages,bookstatus);
+    myLibrary.push(createBook);
+    console.log("This book has now been added in the library: " +createBook.returnBookInfo());
+    const confirmationMsg = createBook.returnBookInfo(); 
+    return myLibrary; 
+};
